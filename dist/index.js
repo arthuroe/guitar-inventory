f4e19bb9adc187eb27c381e0e0ea5c607f72bd61
arthur <arthur.orache@gmail.com>
2020-08-18 10:51:22 +0300

    Add okta authentication

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,45 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
+const sessionAuth = __importStar(require("./middleware/sessionAuth"));
+const routes = __importStar(require("./routes"));
+dotenv_1.default.config();
+const port = process.env.SERVER_PORT;
 const app = express_1.default();
-const port = 8080;
+// confugure express to use EJS
 app.set("views", path_1.default.join(__dirname, "views"));
 app.set("view engine", "ejs");
+// configure session auth
+sessionAuth.register(app);
+// confugure routes
+routes.register(app);
+// start the express server
 app.listen(port, () => {
     // tslint:disable-next-line:no-console
     console.log(`Server started at http://localhost:${port}`);
-});
-app.get("/", (request, response) => {
-    // render index template
-    response.render("index");
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,28 @@
+import dotenv from "dotenv";
 import express from "express";
 import path from "path";
+import * as sessionAuth from "./middleware/sessionAuth";
+import * as routes from "./routes"
+
+
+dotenv.config();
+
+const port = process.env.SERVER_PORT;
 
 const app = express();
-const port = 8080;
 
+// confugure express to use EJS
 app.set( "views", path.join(__dirname, "views" ) );
 app.set( "view engine", "ejs" );
 
+// configure session auth
+sessionAuth.register(app);
+
+// confugure routes
+routes.register(app);
+
+// start the express server
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
   console.log(`Server started at http://localhost:${port}`)
-});
-
-app.get("/", (request, response) => {
-  // render index template
-  response.render("index");
 });
